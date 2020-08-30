01dd314626fb6914c50544cfe7246b56b6b610db
rohansharma06 <rohansharma251998@gmail.com>
2020-08-30 14:49:06 +0530

    add some update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
-import App from "./App";
+import App from "./views/App";
 
 ReactDOM.render(
   <React.StrictMode>
