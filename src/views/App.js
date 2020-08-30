7584f46817a02cdeddca808803bed188a0f5a772
rohansharma06 <rohansharma251998@gmail.com>
2020-08-30 21:27:51 +0530

    update UI

--- src/views/App.js
+++ src/views/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Login from "./Login";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 function App() {
   return <Login />;
 }
+
+// App.propTypes = {
+//   posts: PropTypes.array.isRequired,
+// };
 
 export default App;
