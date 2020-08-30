322f22bd3a4a2c743cd2d20610a129c92ceec890
rohansharma06 <rohansharma251998@gmail.com>
2020-08-30 21:27:36 +0530

    update UI

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
+import { connect } from "react-redux";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import MailOutline from "@material-ui/icons/MailOutline";
 import LockOutlined from "@material-ui/icons/LockOutlined";
 import CustomInput from "../components/CustomInput/CustomInput";
+import Button from "../components/CustomButtons/Button";
+import Danger from "../components/Typography/Danger";
+
+import { login } from "../actions/auth";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const { userPassword, userEmail } = this.props.state;
+    if (userEmail && userPassword) {
+      this.props.dispatch(login(userEmail, userPassword));
+    }
+  };
+
   render() {
+    //const { userPassword, userEmail } = this.props.state;
+    //console.log(userEmail, ":", userPassword);
     return (
       <section className="Form my-5 mx-5">
         <div className="container">
@@ -21,10 +38,10 @@ class Login extends Component {
 
             <div className="col-lg-7 px-5 pt-5">
               <h1 className="font-weight-bold py-3">LogIn</h1>
-              <h4>Enter your's details to log in to your account</h4>
+              <h4>Enter your's details </h4>
               <form>
                 <div className="form-row">
-                  <div className="col-lg-7">
+                  <div className="col-lg-8">
                     <CustomInput
                       labelText="Email"
                       id="success"
@@ -45,7 +62,7 @@ class Login extends Component {
                 </div>
 
                 <div className="form-row">
-                  <div className="col-lg-7">
+                  <div className="col-lg-8">
                     <CustomInput
                       labelText="Password"
                       id="success"
@@ -66,21 +83,25 @@ class Login extends Component {
                 </div>
 
                 <div className="form-row">
-                  <p className="error">*invalid name/username</p>
+                  {/* <p className="error">*invalid name/username</p> */}
+                  <Danger>*invalid name/username</Danger>
                 </div>
                 <div className="form-row">
-                  <div className="col-lg-7">
+                  <div className="col-lg-8">
                     <Button
                       variant="contained"
-                      color="primary"
+                      color="info"
                       className="btn1 mt-3 mb-5"
+                      onClick={this.handleFormSubmit}
                     >
                       Login
                     </Button>
                   </div>
                 </div>
 
-                <a href="#">Forgot password</a>
+                <a href="#">
+                  <Danger>Forgot password!</Danger>
+                </a>
                 <p>
                   Dont have an accoutn? <a href="#">Register here</a>
                 </p>
@@ -93,4 +114,9 @@ class Login extends Component {
   }
 }
 
-export default Login;
+function mapStateToProps(state) {
+  return {
+    state,
+  };
+}
+export default connect(mapStateToProps)(Login);
