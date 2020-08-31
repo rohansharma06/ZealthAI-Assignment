baaef504283cf5050ede468c7fcb91f05a722a1e
rohansharma06 <rohansharma251998@gmail.com>
2020-08-31 16:00:45 +0530

    Update UI

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,44 +1,79 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login } from "../actions/auth";
+import image from "../assets/image/logo.png";
+
+//---- @material-ui/core and /icon
 import InputAdornment from "@material-ui/core/InputAdornment";
 import MailOutline from "@material-ui/icons/MailOutline";
 import LockOutlined from "@material-ui/icons/LockOutlined";
+import People from "@material-ui/icons/PeopleAlt";
+import Icon from "@material-ui/core/Icon";
+
+//---- custom components by creative Tim
 import CustomInput from "../components/CustomInput/CustomInput";
 import Button from "../components/CustomButtons/Button";
 import Danger from "../components/Typography/Danger";
-
-import { login } from "../actions/auth";
+import Info from "../components/Typography/Info";
+import Primary from "../components/Typography/Primary";
+import Warning from "../components/Typography/Warning";
 
 class Login extends Component {
   constructor(props) {
     super(props);
+
+    //---- state to check if both fiels is present or not
+    this.state = {
+      incompleteFilds: false,
+    };
   }
+
+  //---- function to handle login -dispatch action when both email and pass is present
   handleFormSubmit = (e) => {
     e.preventDefault();
     const { userPassword, userEmail } = this.props.state;
+
+    //---- if both felds bresent dispatch action else give invaid fields error
     if (userEmail && userPassword) {
+      this.setState({
+        incompleteFilds: false,
+      });
       this.props.dispatch(login(userEmail, userPassword));
+    } else {
+      this.setState({
+        incompleteFilds: true,
+      });
     }
   };
 
   render() {
-    //const { userPassword, userEmail } = this.props.state;
-    //console.log(userEmail, ":", userPassword);
+    //---- importing data from props and state
+    const { isLoggedin, error, inProgress } = this.props.state;
+    const { incompleteFilds } = this.state;
+
+    //---- if user is login only then redirect to google
+    if (isLoggedin) {
+      return <Redirect to="/google" />;
+    }
     return (
       <section className="Form my-5 mx-5">
-        <div className="container">
+        <div className="container pt-5">
           <div className="row no-gutters">
-            <div className="col-lg-5">
-              <img
-                src="https://images.unsplash.com/photo-1560582861-45078880e48e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-                className="img-fluid"
-                alt="img"
-              />
+            <div className="col-lg-5 side-image">
+              <img src={image} className="img-fluid" alt="logo-img" />
             </div>
 
             <div className="col-lg-7 px-5 pt-5">
-              <h1 className="font-weight-bold py-3">LogIn</h1>
-              <h4>Enter your's details </h4>
+              <Info>
+                Log In
+                <Icon className="login-icon">
+                  <People />
+                </Icon>
+              </Info>
+
+              <Primary my-5>Get started, enter details! </Primary>
+
               <form>
                 <div className="form-row">
                   <div className="col-lg-8">
@@ -65,7 +100,7 @@ class Login extends Component {
                   <div className="col-lg-8">
                     <CustomInput
                       labelText="Password"
-                      id="success"
+                      id="error"
                       success
                       formControlProps={{
                         fullWidth: true,
@@ -82,29 +117,50 @@ class Login extends Component {
                   </div>
                 </div>
 
-                <div className="form-row">
-                  {/* <p className="error">*invalid name/username</p> */}
-                  <Danger>*invalid name/username</Danger>
-                </div>
+                {/* ---- handling errors while login */}
+                {error && (
+                  <div className="form-row">
+                    <Danger>* Error 404: inValid Username/Password</Danger>
+                  </div>
+                )}
+                {incompleteFilds && (
+                  <div className="form-row">
+                    <Danger>* inValid Fields </Danger>
+                  </div>
+                )}
+
                 <div className="form-row">
                   <div className="col-lg-8">
-                    <Button
-                      variant="contained"
-                      color="info"
-                      className="btn1 mt-3 mb-5"
-                      onClick={this.handleFormSubmit}
-                    >
-                      Login
-                    </Button>
+                    {inProgress ? (
+                      <Button
+                        variant="contained"
+                        color="danger"
+                        className="btn1 mt-3 mb-5"
+                        disabled
+                        onClick={this.handleFormSubmit}
+                      >
+                        Log in
+                      </Button>
+                    ) : (
+                      <Button
+                        variant="contained"
+                        color="info"
+                        className="btn1 mt-3 mb-5"
+                        onClick={this.handleFormSubmit}
+                      >
+                        Log in
+                      </Button>
+                    )}
                   </div>
                 </div>
 
                 <a href="#">
-                  <Danger>Forgot password!</Danger>
+                  <Danger>Forgot password! </Danger>
                 </a>
-                <p>
-                  Dont have an accoutn? <a href="#">Register here</a>
-                </p>
+                <Warning>
+                  Dont have an account?
+                  <a href="#"> Register here</a>
+                </Warning>
               </form>
             </div>
           </div>
@@ -114,9 +170,11 @@ class Login extends Component {
   }
 }
 
+//---- access data from store
 function mapStateToProps(state) {
   return {
     state,
   };
 }
+//---- connecting store data to Login components
 export default connect(mapStateToProps)(Login);
