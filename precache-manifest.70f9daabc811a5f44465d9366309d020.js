b3d79036b72dacda7cd3c525206668d3021357d4
rohansharma06 <rohansharma251998@gmail.com>
2020-08-31 16:14:31 +0530

    Updates

---
+++ precache-manifest.70f9daabc811a5f44465d9366309d020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef2dad6cf006db4876546e4c4124c03",
+    "url": "/Zealth-AI-Assignment/index.html"
+  },
+  {
+    "revision": "5cbd387d1cebe67077df",
+    "url": "/Zealth-AI-Assignment/static/css/main.84bf5fd0.chunk.css"
+  },
+  {
+    "revision": "12d71286171d65b7af21",
+    "url": "/Zealth-AI-Assignment/static/js/2.e868d72f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Zealth-AI-Assignment/static/js/2.e868d72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbd387d1cebe67077df",
+    "url": "/Zealth-AI-Assignment/static/js/main.435873a9.chunk.js"
+  },
+  {
+    "revision": "422b05ae9d02f390aef3",
+    "url": "/Zealth-AI-Assignment/static/js/runtime-main.b07e5139.js"
+  },
+  {
+    "revision": "bd3edf143a6e35df1000931c4dfbb676",
+    "url": "/Zealth-AI-Assignment/static/media/logo.bd3edf14.png"
+  }
+]);
