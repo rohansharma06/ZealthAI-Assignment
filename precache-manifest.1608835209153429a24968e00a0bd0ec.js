eefc07743c0ddaf5cf18116381645a80b2c24bfb
rohansharma06 <rohansharma251998@gmail.com>
2020-09-01 06:05:33 +0530

    Updates

---
+++ precache-manifest.1608835209153429a24968e00a0bd0ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06c46a63a7270532010f68b9f96d611",
+    "url": "/ZealthAI-Assignment/index.html"
+  },
+  {
+    "revision": "ed8361b9fe4dae2062d6",
+    "url": "/ZealthAI-Assignment/static/css/main.84bf5fd0.chunk.css"
+  },
+  {
+    "revision": "12d71286171d65b7af21",
+    "url": "/ZealthAI-Assignment/static/js/2.e868d72f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ZealthAI-Assignment/static/js/2.e868d72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8361b9fe4dae2062d6",
+    "url": "/ZealthAI-Assignment/static/js/main.2325e04e.chunk.js"
+  },
+  {
+    "revision": "a71749d80e618e20f328",
+    "url": "/ZealthAI-Assignment/static/js/runtime-main.3ddb8e8e.js"
+  },
+  {
+    "revision": "bd3edf143a6e35df1000931c4dfbb676",
+    "url": "/ZealthAI-Assignment/static/media/logo.bd3edf14.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5915113663b5ddeb4d9ac63ae66c521d.js"
+  "/ZealthAI-Assignment/precache-manifest.1608835209153429a24968e00a0bd0ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZealthAI-Assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
